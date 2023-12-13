9f67c769e228fb006df538e5a2e56f0b4d0af637
Ihor Savenko <csigorek@gmail.com>
2023-12-13 14:22:17 +0000

    Added QR Scanner

--- bot/handlers/message.py
+++ bot/handlers/message.py
@@ -1,7 +1,10 @@
 from aiogram import Dispatcher
 
-from bot.controllers.message import custom_check
+from bot.controllers.message import custom_check, image_qr_recognition
 
 
 def setup(dp: Dispatcher):
     dp.register_message_handler(custom_check, regexp=r"^\d{7}$", state="*")
+    dp.register_message_handler(
+        image_qr_recognition, content_types=["photo"], state="*"
+    )
